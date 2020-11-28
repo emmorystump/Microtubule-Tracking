7fbc201b5b529e5ae193f3047fe53254721fd009
Emmory Stump <emmorystump@wustl.edu>
2020-11-28 14:31:39 -0600

    oh lol working for tiff but not tif?

--- microtubules.py
+++ microtubules.py
@@ -1,9 +1,9 @@
 from cv2 import cv2
 
 
-filepath = "/microtubule_videos/Dynamics_microtubules2.tif"
+filepath = "microtubule_videos/1380 nM SPR1GFP WT - 13_XY1500063595_Z0_T000_C1.tiff"
 
 # Probs need to change flags- just used ones from stackoverflow
 ret, images = cv2.imreadmulti(filepath, [], cv2.IMREAD_ANYCOLOR)
 
-print(ret)
+print(images)
