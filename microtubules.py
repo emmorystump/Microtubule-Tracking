23933b4aaaaa67039cca9ab61f596a60b4211c67
Emmory Stump <emmorystump@wustl.edu>
2020-11-28 14:29:42 -0600

    nvm not loading lol

--- microtubules.py
+++ microtubules.py
@@ -1,7 +1,9 @@
 from cv2 import cv2
 
 
-filepath = "microtubule_videos/Dynamics microtubules2.tif"
+filepath = "/microtubule_videos/Dynamics_microtubules2.tif"
 
 # Probs need to change flags- just used ones from stackoverflow
-ret, images = cv2.imreadmulti(filename = filepath, flags = cv2.IMREAD_ANYCOLOR )
+ret, images = cv2.imreadmulti(filepath, [], cv2.IMREAD_ANYCOLOR)
+
+print(ret)
