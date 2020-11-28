159b2b007c7a8c70b1d8e18f7c84b6a12c4896ab
Emmory Stump <emmorystump@wustl.edu>
2020-11-28 14:36:18 -0600

    hiya

--- microtubules.py
+++ microtubules.py
@@ -1,4 +1,7 @@
 from cv2 import cv2
+import numpy as np
+
+np.set_printoptions(threshold=np.inf)
 
 
 filepath = "microtubule_videos/1380 nM SPR1GFP WT - 13_XY1500063595_Z0_T000_C1.tiff"
@@ -6,4 +9,4 @@ filepath = "microtubule_videos/1380 nM SPR1GFP WT - 13_XY1500063595_Z0_T000_C1.t
 # Probs need to change flags- just used ones from stackoverflow
 ret, images = cv2.imreadmulti(filepath, [], cv2.IMREAD_ANYCOLOR)
 
-print(images)
+print(images[0])
