34c99a1049a0d69ac52760b87648b6fd339f8cf1
Emmory Stump <emmorystump@wustl.edu>
2020-12-06 16:28:21 -0600

    fixed

--- GUI_1.py
+++ GUI_1.py
@@ -84,14 +84,14 @@ class App:
     
         # self.first_frame = cv2.GaussianBlur(self.first_frame,(5,5),0)
 
-        self.first_frame = cv2.adaptiveThreshold(self.first_frame, self.first_frame.max(), cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 10, -10)
+        self.first_frame = cv2.adaptiveThreshold(self.first_frame, self.first_frame.max(), cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 15, -10)
         # self.first_frame = cv2.adaptiveThreshold(self.first_frame, self.first_frame.max(), cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
 
         kernel = np.ones((3, 3), np.uint8)
         kernel2 = np.ones((2, 2), np.uint8)
 
         # self.first_frame = cv2.morphologyEx(self.first_frame, cv2.MORPH_CLOSE, kernel)
-        self.first_frame = cv2.erode(self.first_frame, kernel2,iterations = 1)
+        # self.first_frame = cv2.erode(self.first_frame, kernel2,iterations = 2)
         # self.first_frame = cv2.morphologyEx(self.first_frame, cv2.MORPH_OPEN, kernel)
 
         # self.first_frame[self.first_frame==255] = 1
@@ -100,6 +100,7 @@ class App:
 
         num_labels, labels_im, stats, centroids = cv2.connectedComponentsWithStats(self.first_frame, 8, cv2.CV_32S)
         print(num_labels)
+
 
         self.photo = ImageTk.PhotoImage(image=Image.fromarray(self.first_frame))
         self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
